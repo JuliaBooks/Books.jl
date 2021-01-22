714dd000481185b31e77991ba4b6dea98a26e76b
Rik Huijzer <t.h.huijzer@rug.nl>
2021-01-22 20:37:07 +0100

    Setup CI dependencies via Julia (#8)

--- src/Books.jl
+++ src/Books.jl
@@ -3,5 +3,6 @@ module Books
 include("html.jl")
 include("build.jl")
 include("serve.jl")
+include("ci.jl")
 
 end # module
---
+++ src/ci.jl
@@ -0,0 +1,46 @@
+is_github_ci() = "CI" in keys(ENV)
+
+function install_via_tar()
+    @assert is_github_ci()
+    PANDOC_VERSION = "2.10.1"
+    CROSSREF_VERSION = "0.3.8.1"
+
+    filename = "pandoc-$PANDOC_VERSION-1-amd64.deb"
+    download("https://github.com/jgm/pandoc/releases/download/$PANDOC_VERSION/$filename", filename)
+    run(`sudo dpkg -i $filename`)
+
+    filename = "pandoc-crossref-Linux.tar.xz"
+    download("https://github.com/lierdakil/pandoc-crossref/releases/download/v$CROSSREF_VERSION/$filename", filename)
+    run(`tar -xf $filename`)
+    bin_dir = joinpath(homedir(), "bin")
+    mkdir(bin_dir)
+    name = "pandoc-crossref"
+    mv(name, joinpath(bin_dir, name))
+    open(ENV["GITHUB_PATH"], "a") do io
+        write(io, bin_dir)
+    end
+end
+
+function install_apt_packages()
+    @assert is_github_ci()
+    packages = [
+        "make", 
+        "pdf2svg", 
+        "texlive-fonts-recommended", 
+        "texlive-fonts-extra",
+        "texlive-latex-base",
+        "texlive-binaries",
+        "texlive-xetex"
+    ]
+
+    run(`sudo apt-get -qq update`)
+    for package in packages
+        println("Installing $package via apt")
+        run(`sudo apt-get install -y $package`)
+    end
+end
+
+function install_dependencies()
+    install_via_tar()
+    install_apt_packages()
+end
