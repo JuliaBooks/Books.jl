753ef9e1096c7fc739a1fe42f151959724ec6ab3
Rik Huijzer <rikhuijzer@pm.me>
2021-03-07 16:43:07 +0100

    Install extra font

--- src/ci.jl
+++ src/ci.jl
@@ -6,7 +6,7 @@ Check whether we need sudo.
 This differs between GitHub and GitLab CI.
 """
 function is_sudo_env()
-    try 
+    try
         run(`sudo echo This text is printed with sudo privileges`)
         return true
     catch
@@ -17,6 +17,22 @@ sudo_prefix() = is_sudo_env() ? "sudo" : ""
 function nonempty_run(args::Vector)
     filter!(!=(""), args)
     run(`$args`)
+end
+
+"""
+    install_extra_fonts()
+
+Required for Source Code Pro.
+Thanks to https://github.com/AnomalyInnovations/serverless-stack-com.
+"""
+function install_extra_fonts()
+    font_repo_dir = joinpath(homedir(), "source-code-pro")
+    run(`git clone https://github.com/adobe-fonts/source-code-pro $font_repo_dir`)
+    ttf_dir = joinpath(font_repo_dir, "TTF")
+    fonts_dir = joinpath(homedir(), ".fonts")
+    mkpath(fonts_dir)
+    args = [sudo, "cp", "$ttf_dir/*.ttf", fonts_dir]
+    nonempty_run(args)
 end
 
 function install_apt_packages()
@@ -45,7 +61,7 @@ function install_apt_packages()
 end
 
 function validate_installation(name::AbstractString; args="--help")
-    try 
+    try
         run(`$name $args`)
     catch e
         error("Could not run $name with args $args")
@@ -90,4 +106,5 @@ end
 function install_dependencies()
     install_apt_packages()
     install_non_apt_packages()
+    install_extra_fonts()
 end
