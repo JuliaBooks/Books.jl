24950a8c375461806cc77721d9d682dae59bc004
Rik Huijzer <rikhuijzer@pm.me>
2021-03-09 22:41:41 +0100

    Fix CI?

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,15 +4,13 @@ using Test
 
 B = Books
 
-if v"1.6.0-rc1" ≤ VERSION
-    DocMeta.setdocmeta!(
-      Books,
-      :DocTestSetup,
-      :(using Books; using DataFrames; mkpath(Books.GENERATED_DIR));
-      recursive=true
-    )
-    doctest(Books)
-end
+DocMeta.setdocmeta!(
+  Books,
+  :DocTestSetup,
+  :(using Books; using DataFrames; mkpath(Books.GENERATED_DIR));
+  recursive=true
+)
+doctest(Books)
 
 include("output.jl")
 include("html.jl")
