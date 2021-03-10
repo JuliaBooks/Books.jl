5c7e95097e70aae2db7196a97aac76bc64fd7afb
Rik Huijzer <rikhuijzer@pm.me>
2021-03-10 15:26:25 +0100

    Fix serve always using default project

--- src/serve.jl
+++ src/serve.jl
@@ -51,7 +51,7 @@ function serve(simplewatcher=default_simplewatcher();
     if !isdir(dir)
         mkpath(dir)
     end
-    html()
+    html(project)
     port = config(project)["port"]
     LiveServer.serve(simplewatcher; verbose, port, dir)
 end
