6e5245a03b23265ec9fdcc2de7f1f22a87d5c4f4
Rik Huijzer <rikhuijzer@pm.me>
2021-04-12 13:44:38 +0200

    Remove redundant include statement

--- src/outputs/gadfly.jl
+++ src/outputs/gadfly.jl
@@ -1,7 +1,5 @@
 @debug "Loading Gadfly support into Books via Requires"
 
-include("compose.jl")
-
 function convert_output(path, out::Gadfly.Plot; kwargs...)
     convert_gadfly_output(path, out; kwargs...)
 end
