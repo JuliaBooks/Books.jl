69d846c6b1f61b99ec2051db088bb68866b9dc1b
Rik Huijzer <rikhuijzer@pm.me>
2021-04-12 13:41:38 +0200

    Use kwargs... for Gadfly.Plot

--- src/outputs/gadfly.jl
+++ src/outputs/gadfly.jl
@@ -1,6 +1,7 @@
+@debug "Loading Gadfly support into Books via Requires"
+
 include("compose.jl")
 
-function convert_output(path, out::Gadfly.Plot;
-        caption=nothing, label=nothing, width=nothing, height=nothing)
-    convert_gadfly_output(path, out; caption, label, width, height)
+function convert_output(path, out::Gadfly.Plot; kwargs...)
+    convert_gadfly_output(path, out; kwargs...)
 end
