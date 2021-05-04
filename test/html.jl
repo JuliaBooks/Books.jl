91df6b39cb74fec9772c06e1027b73865a09d49d
Jun Tian <tianjun.cpp@gmail.com>
2021-05-04 23:02:33 +0800

    revert unnecessary format

--- test/html.jl
+++ test/html.jl
@@ -70,12 +70,12 @@
         @test url_prefix != ""
         actual = Books.fix_links(names, pages, url_prefix) |> last |> first
         expected = """
-                        <!DOCTYPE html>
-                        <link rel="stylesheet" href="/Books.jl/files/style.css"/>
-                        <a href="/Books.jl/test.html#sec:foo">Foo</a>
-                        <h2 data-number="3.5" id="sec:foo"><span class="header-section-number">3.5</span> Foo</h2>
-                        <p> <a href="https://www.google.com">Jump</a></p>
-                        """
+            <!DOCTYPE html>
+            <link rel="stylesheet" href="/Books.jl/files/style.css"/>
+            <a href="/Books.jl/test.html#sec:foo">Foo</a>
+            <h2 data-number="3.5" id="sec:foo"><span class="header-section-number">3.5</span> Foo</h2>
+            <p> <a href="https://www.google.com">Jump</a></p>
+            """
         @test actual == expected
     end
     url_prefix = ""
