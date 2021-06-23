faf70ceafc3d1d53cbd7419607f37425b04f055d
Rik Huijzer <rikhuijzer@pm.me>
2021-06-23 18:12:43 +0200

    Set M = BooksDocs in CI

--- docs/src/BooksDocs.jl
+++ docs/src/BooksDocs.jl
@@ -20,7 +20,7 @@ plot = Plots.plot
 include("includes.jl")
 
 function build()
-    Books.gen(; M=BooksDocs, fail_on_error=true)
+    Books.gen(; fail_on_error=true)
     extra_head = "<!-- Example comment placed in the html header. -->"
     Books.build_all(; extra_head)
 end
