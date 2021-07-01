f1c08af36fd3a62e8e13f4912e6a87a2de1023c0
Rik Huijzer <rikhuijzer@pm.me>
2021-07-01 23:37:19 +0200

    Minor tweaks

--- src/html.jl
+++ src/html.jl
@@ -139,13 +139,16 @@ function previous_and_next_buttons(body::String, menu_items::Vector{String}, i::
     max = length(menu_items)
     prev = 2 < i ? menu_items[i - 2] : ""
     prev = strip(prev)
+    text_prev = prev == "" ? "" : "(key ←)"
     next = i < max ? menu_items[i] : ""
     next = strip(next)
+    text_next = next == "" ? "" : "(key →)"
+    # The margins are needed for situations where only next page link is available.
     keyboard_shortcut_text = """
-        <p id="nav-prev" style="text-align: left; margin-top: 0em;">
-            (key ←)
-            <span id="nav-next" style="float: right;">
-                (key →)
+        <p id="nav-prev" style="text-align: left; margin-top: 0em; margin-left: 0.4em;">
+            $text_prev
+            <span id="nav-next" style="float: right; margin-right: 0.4em;">
+                $text_next
             </span>
         </p>
     """
