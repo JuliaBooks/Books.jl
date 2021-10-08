c9cf1afaaf0c9d8e5a4bd106980afe2e07dc532c
rikhuijzer <rikhuijzer@pm.me>
2021-10-08 15:01:31 +0200

    Set default back to archive.org

--- src/build.jl
+++ src/build.jl
@@ -393,13 +393,15 @@ function build_all(; project="default", extra_head="", fail_on_error=false)
     html(; project, extra_head, fail_on_error, build_sitemap)
     write_extra_html_files(project)
     try
-        # Temporary test.
-        patch_tectonic_url()
         pdf(; project)
-    catch
+    catch e
+        # Only patch in CI.
         if IS_CI
+            println("PDF generation failed. Patching the archive.org URL and trying again")
             patch_tectonic_url()
             pdf(; project)
+        else
+            throw(e)
         end
     end
     docx(; project)
