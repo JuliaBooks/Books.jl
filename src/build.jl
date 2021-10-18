405d9ba82c235f5c58afaea4b7604fe188090352
Rik Huijzer <rikhuijzer@pm.me>
2021-10-18 17:09:25 +0200

    Bump highlight to version 11.3.1 (#242)

--- src/build.jl
+++ src/build.jl
@@ -237,7 +237,7 @@ function ci_url_prefix(project)
 end
 
 @memoize function highlight(url_prefix)
-    highlight_dir = joinpath(Artifacts.artifact"Highlight", "cdn-release-11.1.0")
+    highlight_dir = joinpath(Artifacts.artifact"Highlight", "cdn-release-11.3.1")
 
     highlight_name = "highlight.min.js"
     highlight_path = joinpath(highlight_dir, "build", highlight_name)
