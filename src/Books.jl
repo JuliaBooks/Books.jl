b690429963c9d60eb9fd4384b9e8964de4ef5f4e
Rik Huijzer <rikhuijzer@pm.me>
2022-08-10 20:48:45 +0200

    Avoid `Requires.jl` (#285)

--- src/Books.jl
+++ src/Books.jl
@@ -20,7 +20,6 @@ using Dates: today
 using InteractiveUtils: gen_call_with_extracted_types
 using Markdown: MD
 using Memoize: @memoize
-using Requires: @require
 using pandoc_crossref_jll: pandoc_crossref_path
 using pandoc_jll: pandoc
 
@@ -48,11 +47,5 @@ export code_block, output_block
 export @sc, sc, CodeAndFunction, @sco, sco, scob
 export gen
 export serve
-
-function __init__()
-    @require AlgebraOfGraphics="cbdf2221-f076-402e-a563-3d30da359d67" include("outputs/aog.jl")
-    @require Makie="ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" include("outputs/makie.jl")
-    @require Plots="91a5bcdd-55d7-5caf-9e0b-520d859cae80" include("outputs/plots.jl")
-end
 
 end # module
