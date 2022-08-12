53b2fbb7482f00d7b19bd1b9214819be5a7d6de8
Rik <rikhuijzer@pm.me>
2022-08-12 20:24:11 +0200

    Handle GLMakie correctly

--- docs/src/BooksDocs.jl
+++ docs/src/BooksDocs.jl
@@ -8,29 +8,36 @@ import TOML
 
 using Reexport
 @reexport using Books
-@reexport using CairoMakie
+@reexport using CairoMakie:
+    Axis,
+    CairoMakie,
+    lines,
+    scatter,
+    scatterlines!
 @reexport using CodeTracking
 @reexport using DataFrames
 @reexport using Dates
-@reexport using Plots
+@reexport using Plots: Plot, plot, savefig
 
-plot = Plots.plot
-scatter = CairoMakie.scatter
+using CairoMakie.Makie: Figure, FigureAxisPlot
 
-const MAKIE_PLOT_TYPES = Union{CairoMakie.Makie.Figure, CairoMakie.Makie.FigureAxisPlot}
-Books.is_image(plot::MAKIE_PLOT_TYPES) = true
-
-function makie_save(path::String, p)
-    px_per_unit = 3
-    CairoMakie.FileIO.save(path, p; px_per_unit)
+const MAKIE_PLOT_TYPES = Union{Figure, FigureAxisPlot}
+function _makie_save(path::String, p)
+    try
+        # SVG will fail with GLMakie.
+        FileIO.save(path, p; px_per_unit=3)
+    catch
+        # It doesn't matter since Books.jl will only use SVG if available, otherwise PNG.
+    end
 end
 
-Books.svg(svg_path::String, p::MAKIE_PLOT_TYPES) = makie_save(svg_path, p)
-Books.png(png_path::String, p::MAKIE_PLOT_TYPES) = makie_save(png_path, p)
+Books.is_image(plot::MAKIE_PLOT_TYPES) = true
+Books.svg(svg_path::String, p::MAKIE_PLOT_TYPES) = _makie_save(svg_path, p)
+Books.png(png_path::String, p::MAKIE_PLOT_TYPES) = _makie_save(png_path, p)
 
-Books.is_image(plot::Plots.Plot) = true
-Books.svg(svg_path::String, p::Plots.Plot) = savefig(p, svg_path)
-Books.png(png_path::String, p::Plots.Plot) = savefig(p, png_path)
+Books.is_image(plot::Plot) = true
+Books.svg(svg_path::String, p::Plot) = savefig(p, svg_path)
+Books.png(png_path::String, p::Plot) = savefig(p, png_path)
 
 include("includes.jl")
 
