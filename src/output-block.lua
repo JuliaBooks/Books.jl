c9f41a7ceacf551a6a4c58b6d1b3d6effdbb0e09
Yusheng Zhao <rpfzpp@gmail.com>
2024-01-17 17:57:58 +0800

    Bump Pandoc to 2.19 (#315)
    Hello @rikhuijzer , we are trying to use Books.jl and would like for it to be able to build the site on Apple Silicon Machines. I did the following to make it work
    
    1. Bump package version of `pandoc_jll` , `pandoc_corssref_jll`, `tectonic_jll`. 
    2. Fixed [Lua script](https://github.com/JuliaBooks/Books.jl/blob/fb37b180728af070ecb7985e47b7a59577eb81c9/src/output-block.lua#L8C1-L8C1) for supporting pandoc >= 2.16
    
    Fixes #309.

--- src/output-block.lua
+++ src/output-block.lua
@@ -2,11 +2,12 @@
 -- This function manually handles blocks tagged as output.
 --
 -- Thanks to https://github.com/jgm/pandoc/issues/4116#issuecomment-579426694.
+-- Thanks to https://github.com/jgm/pandoc/issues/4116#issuecomment-1020677402 for support of pandoc >= 2.16
 --
 local function CodeBlock (elem)
-    if elem.c[1][2][1] == "language-julia" then
+    if elem.classes[1] == "language-julia" then
         return pandoc.RawBlock("latex", "\n\\begin{lstlisting}[language=Julia]\n"..elem.text.."\n\\end{lstlisting}\n")
-    elseif elem.c[1][2][1] == "output" then
+    elseif elem.classes[1] == "output" then
         return pandoc.RawBlock("latex", "\n\\begin{lstlisting}[language=Output]\n"..elem.text.."\n\\end{lstlisting}\n")
     else
         return elem
